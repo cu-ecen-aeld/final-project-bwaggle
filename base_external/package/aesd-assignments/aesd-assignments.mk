63a94f8ca2c4a97349ec44052e0175625eca05f4
bwaggle <bwaggle@Ubuntu.myguest.virtualbox.org>
2023-10-04 06:14:47 -0500

    Removed aesdchar driver

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = f7209270de88e20a797cd2cba57c018aa6e20549
+AESD_ASSIGNMENTS_VERSION = 131964f85b8667ff4f01e024418d0a25690473f8
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
